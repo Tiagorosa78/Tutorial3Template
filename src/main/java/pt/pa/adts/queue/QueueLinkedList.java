a9c10138d7c74e0203101f69e94ec319dbd0eac9
Luís Damas <samad@irisdata.pt>
2023-10-14 13:33:54 +0100

    Change generic name to T

--- src/main/java/pt/pa/adts/queue/QueueLinkedList.java
+++ src/main/java/pt/pa/adts/queue/QueueLinkedList.java
@@ -2,9 +2,9 @@ package pt.pa.adts.queue;
 
 /**
  * This class must implement the Queue interface
- * @param <E>
+ * @param <T>
  */
-public class QueueLinkedList<E>  {
+public class QueueLinkedList<T>  {
 
     private ListNode header, trailer;
     private int size;
@@ -16,11 +16,11 @@ public class QueueLinkedList<E>  {
     //TODO: implementar métodos da interface à custa da estrutura de dados fornecida
 
     private class ListNode {
-        private E element;
+        private T element;
         private ListNode next;
         private ListNode prev;
 
-        public ListNode(E element, ListNode prev, ListNode next) {
+        public ListNode(T element, ListNode prev, ListNode next) {
             this.element = element;
             this.next = next;
             this.prev = prev;
